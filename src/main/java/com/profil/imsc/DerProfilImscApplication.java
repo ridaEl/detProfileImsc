bf50eb78f0c20b157d87943412b94f02c4f98574
rida <rida.elbagar@gmail.com>
2019-05-24 22:20:11 +0000

    initialisation du projet

---
+++ src/main/java/com/profil/imsc/DerProfilImscApplication.java
@@ -0,0 +1,13 @@
+package com.profil.imsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerProfilImscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerProfilImscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profil/imsc/DerProfilImscApplicationTests.java
@@ -0,0 +1,16 @@
+package com.profil.imsc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DerProfilImscApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
