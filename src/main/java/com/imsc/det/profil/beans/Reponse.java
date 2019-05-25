65a66695b7056335c8e970d699e05b2ef67d7608
PC <PC@192.168.1.2>
2019-05-25 17:30:45 +0000

    hadchi li tzad, Question 9aditha chwya, o reponse

---
+++ src/main/java/com/imsc/det/profil/beans/Reponse.java
@@ -0,0 +1,72 @@
+package com.imsc.det.profil.beans;
+
+import java.util.Collection;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+public class Reponse {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	@Column(name="idQuestion")
+	
+	private Long idRep;
+	
+	@Column(name="IntituleRep")
+	private String IntituleRep;
+	
+	@Column(name="Score")
+	private int Score;
+	
+	@ManyToMany
+	@JoinTable(name="Question")
+	private Collection<Question> questions;
+
+	public Reponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reponse(String intituleRep, int score) {
+		super();
+		IntituleRep = intituleRep;
+		Score = score;
+	}
+
+	public Long getIdRep() {
+		return idRep;
+	}
+
+	public void setIdRep(Long idRep) {
+		this.idRep = idRep;
+	}
+
+	public String getIntituleRep() {
+		return IntituleRep;
+	}
+
+	public void setIntituleRep(String intituleRep) {
+		IntituleRep = intituleRep;
+	}
+
+	public int getScore() {
+		return Score;
+	}
+
+	public void setScore(int score) {
+		Score = score;
+	}
+
+	public Collection<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(Collection<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+}
