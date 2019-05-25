7566c7eda9dbb5a494d9ab416eb41654589422a8
PC <PC@192.168.1.2>
2019-05-25 15:52:02 +0000

    Class Question b mapping + fichier application.properties

---
+++ src/main/java/com/imsc/det/profil/beans/Question.java
@@ -0,0 +1,57 @@
+package com.imsc.det.profil.beans;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Question implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="idQuestion")
+	private Long idQuest;
+	@Column(name="Intitule")
+	private String IntituleQuest;
+	@OneToOne
+	@JoinTable(name="Categorie")
+	private Categorie Cat;
+	@OneToMany
+	@JoinTable(name="Reponse")
+	private Set<Reponse> reponses;
+	
+	public Question(String intituleQuest, Categorie cat) {
+		super();
+		IntituleQuest = intituleQuest;
+		Cat = cat;
+	}
+	public Question() {
+		super();
+	}
+	public Long getIdQuest() {
+		return idQuest;
+	}
+	public void setIdQuest(Long idQuest) {
+		this.idQuest = idQuest;
+	}
+	public String getIntituleQuest() {
+		return IntituleQuest;
+	}
+	public void setIntituleQuest(String intituleQuest) {
+		IntituleQuest = intituleQuest;
+	}
+	public Categorie getCat() {
+		return Cat;
+	}
+	public void setCat(Categorie cat) {
+		Cat = cat;
+	}
+	public Set<Reponse> getReponses() {
+		return reponses;
+	}
+	public void setReponses(Set<Reponse> reponses) {
+		this.reponses = reponses;
+	}
+
+}
